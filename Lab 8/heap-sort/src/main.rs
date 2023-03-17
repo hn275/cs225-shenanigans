4fdb449b0304e62c59ac19ad79f2876e9fc33f5a
Hal <haln_01@proton.me>
2023-03-17 14:59:11 -0700

    feat: heap sort

--- Lab 8/heap-sort/src/main.rs
+++ Lab 8/heap-sort/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
     let mut arr: Vec<i32> = vec![99, 19, 9, 7, 11, 3, 3, 2, 2, 1];
     println!("Before sort {arr:?}");
-
     heap_sort(&mut arr);
-
     println!("After sort: {arr:?}");
 }
 
@@ -13,10 +11,12 @@ fn heap_sort(arr: &mut Vec<i32>) {
     swap(arr, 0, end_index);
     end_index -= 1;
     maxify_heap(arr, 0, end_index);
+
     loop {
         swap(arr, 0, end_index);
         end_index -= 1;
         maxify_heap(arr, 0, end_index);
+
         if end_index == 1 {
             break;
         }
@@ -24,25 +24,25 @@ fn heap_sort(arr: &mut Vec<i32>) {
 }
 
 fn maxify_heap(arr: &mut Vec<i32>, start_idx: usize, end_idx: usize) {
-    let left_index = 2 * start_idx + 1;
-    let right_index = 2 * start_idx + 2;
+    let left_node_idx = 2 * start_idx + 1;
+    let right_node_idx = 2 * start_idx + 2;
 
-    let mut largest_index: usize;
+    let mut largest_index: usize = start_idx;
 
-    if left_index < end_idx && arr[left_index] > arr[start_idx] {
-        largest_index = left_index;
-    } else {
-        largest_index = start_idx;
+    if left_node_idx < end_idx && arr[left_node_idx] > arr[start_idx] {
+        largest_index = left_node_idx;
     }
 
-    if right_index < end_idx && arr[right_index] > arr[largest_index] {
-        largest_index = right_index;
+    if right_node_idx < end_idx && arr[right_node_idx] > arr[largest_index] {
+        largest_index = right_node_idx;
     }
 
-    if largest_index != start_idx {
-        swap(arr, start_idx, largest_index);
-        maxify_heap(arr, largest_index, end_idx);
-    }
+    if largest_index == start_idx {
+        return;
+    };
+
+    swap(arr, start_idx, largest_index);
+    maxify_heap(arr, largest_index, end_idx);
 }
 
 fn swap(arr: &mut Vec<i32>, idx1: usize, idx2: usize) {
