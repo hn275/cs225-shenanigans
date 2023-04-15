bc170601ac28928b01db0b5a58ca7c6eea02f283
Hal <haln_01@proton.me>
2023-04-15 15:26:07 -0700

    assingment 6 done

--- assignment 6/WayFinder.java
+++ assignment 6/WayFinder.java
@@ -9,36 +9,98 @@ import java.util.*;
 
 
 public class WayFinder {
-	
-	
-    static int shortestPath(char[][] map){
-		for (char[] i : map) {
-			System.out.println(i);
+	static private class Point {
+		int x;
+		int y;
+		int dist;
+
+		Point(int x, int y, int dist) {
+			this.x = x;
+			this.y = y;
+			this.dist = dist;
 		}
-		for (int i = 0; i < map.length; ++i) {
-			for (int j = 0; j < map[i].length; ++j) {
-				if (map[i][j] == 'A') {
-					System.out.print(i);
-					System.out.print(j);
+
+		private Point move(Point loc) {
+			return new Point(
+				this.x + loc.x, 
+				this.y + loc.y, 
+				this.dist + loc.dist
+			);
+		}
+
+		private void update(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		private boolean outOfBound(int size) {
+			return this.x < 0 
+				|| this.y < 0 
+				|| this.x >= size 
+				|| this.y >= size;
+		}
+	}
+
+	static int shortestPath(char[][] map){
+		// Locating origin
+		Point origin = new Point(-1, -1, 0);
+
+		for (int y = 0; y < map.length; ++y) {
+			boolean found = false;
+			for (int x = 0; x < map[y].length; ++x) {
+			if (map[y][x] == 'A') {
+					origin.update(x, y);
+					found = true;
+					break;
 				}
 			}
+			if (found) break;
 		}
-		
-		/*
-		// Variable for distance of shortest path between A and B.
-		int distance = Integer.MAX_VALUE;
-		System.out.println(distance);
-		
-         Your solution goes here.
-        */
-		 
-		 
-		return 2;
-        
-    }
-	
-    public static void main(String[] args) {
-		System.out.println("HEllo");
+
+		if (origin.x == -1 || origin.y == -1) return Integer.MAX_VALUE;
+
+		// BFS
+		Point[] DIRECTIONS = {
+			new Point(-1, 0, 1), // left
+			new Point(1, 0, 1), // right
+			new Point(0, -1, 1), // up
+			new Point(0, 1, 1), // down
+		};
+
+		int size = map.length;
+		boolean[][] seen = new boolean[size][size];
+
+		LinkedList<Integer> paths = new LinkedList<>();
+
+		Queue<Point> queue = new LinkedList<>();
+		queue.offer(origin);
+		seen[origin.y][origin.x] = true;
+
+
+		while (!queue.isEmpty()) {
+			Point currentNode = queue.poll();
+
+			for (Point direction : DIRECTIONS) {
+				Point nextNode = currentNode.move(direction);
+
+				if (nextNode.outOfBound(size)) continue;
+
+				boolean pathBlocked = map[nextNode.y][nextNode.x] == '#';
+				if (seen[nextNode.y][nextNode.x] || pathBlocked) continue;
+
+				queue.offer(nextNode);
+				seen[nextNode.y][nextNode.x] = true;
+
+				if (map[nextNode.y][nextNode.x] == 'B') {
+					return nextNode.dist;
+				}
+			}
+
+		}
+		return Integer.MAX_VALUE;
+	}
+
+	public static void main(String[] args) {
 		Scanner s;
 		if (args.length > 0){
 			try{
@@ -52,21 +114,21 @@ public class WayFinder {
 			s = new Scanner(System.in);
 			System.out.printf("Reading input values from stdin.\n");
 		}
-		
+
 		int n = s.nextInt();
-		char[][] map = new char[n][n];
+	char[][] map = new char[n][n];
 		for (int i = 0; i < n; i++){
 			String temp = s.next();
 			for (int j = 0; j < n; j++){
 				map[i][j] = temp.charAt(j);
 			}
 		}	
-		
+
 		int distance = shortestPath(map);
-		
+
 		if (distance < Integer.MAX_VALUE)
-			System.out.println(distance);
+		System.out.println(distance);
 		else
-			System.out.println("IMPOSSIBLE");
-    }
+		System.out.println("IMPOSSIBLE");
+	}
 }
--- assignment 6/run.sh
+++ assignment 6/run.sh
@@ -1,3 +1,10 @@
-javac WayFinder.java
-java WayFinder ./TestCases/input/input00.txt
-rm ./WayFinder.class
+run() {
+    for file in ./TestCases/input/*; do
+        java WayFinder $file
+        echo "--"
+    done
+}
+
+
+# javac WayFinder.java && java WayFinder ./TestCases/input/input00.txt && rm ./WayFinder.class
+javac WayFinder.java && run && rm ./*.class
